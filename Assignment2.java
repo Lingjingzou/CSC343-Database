2d3a4318036b0ccb8fcb6abdd809195aca0e6735
Lingjing Zou <lingjing.zou@mail.utoronto.ca>
2019-11-12 22:48:10 -0500

    new change

--- Assignment2.java
+++ Assignment2.java
@@ -76,13 +76,13 @@ public class Assignment2 {
          // String queryString = "INSERT INTO available (driverID, when, location)" 
          //                      + " VALUES (?, ?, ?)";
          String queryString = "INSERT INTO available VALUES" +
-                           "(12345, '2016-01-08 04:05', '(1, 2)');";
+                           "(?, ?, ?);";
          PreparedStatement ps = connection.prepareStatement(queryString);
          // ps.setInt(1, driverID);
          // System.err.println("Set driver ID!");
          // ps.setTimestamp(2, when);
          // ps.setObject(3, location);
-         ps.executeQuery();
+         ps.executeUpdate();
          return true;
 
       } catch (SQLException e) {
